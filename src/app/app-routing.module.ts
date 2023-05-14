9ea904aa48e158c0134e3a3ac026939cc868d03f
smeurares <smeurares@gmail.com>
2023-05-14 14:14:59 +0300

    initial setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: "auth",
+  loadChildren: () => import('./core/core.module').then(m => m.CoreModule)
+},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { CoreRoutingModule } from './core-routing.module';
+import { ShopComponent } from './shop/shop.component';
+
+
+
+@NgModule({
+  declarations: [
+    LoginComponent,
+    RegisterComponent,
+    ShopComponent
+  ],
+  imports: [
+    CommonModule,
+    CoreRoutingModule
+  ]
+})
+export class CoreModule { }
